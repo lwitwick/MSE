96ef690bfd48fab310c79992a1cb9601469d4143
kg-barnes <kgbarnes2003@gmail.com>
2025-02-14 13:40:54 -0500

    Reorganized the rectilinear lab and added torsional data

---
+++ Lab 1/Lab 1 Torsional/MSE_lab1_t_2.m
@@ -0,0 +1,12 @@
+clear all
+close all
+clc
+
+% ----------------------------------------------------
+
+% 24-452 MSE Spring 2025 Group B3 
+% LAB 1 - Torsional - Experiment 2
+fprintf('\n24-452 MSE Lab 1\n');
+
+% ----------------------------------------------------
+% Data Import
