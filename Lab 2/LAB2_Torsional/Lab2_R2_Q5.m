04c7f02db1e3a2247b549b57950a6e42899c122c
kg-barnes <kgbarnes2003@gmail.com>
2025-03-16 18:23:00 -0400

    Torsional Q5

--- Lab 2/LAB2_Torsional/Lab2_R2_Q5.m
+++ Lab 2/LAB2_Torsional/Lab2_R2_Q5.m
@@ -9,22 +9,25 @@ clc
 fprintf('\n24-452 MSE Lab 2\n');
 
 % make current folder
-cd('C:\Users\kgbar\Documents\MSE\MSE\Lab 2\LAB2_Rectilinear')
+cd('C:\Users\kgbar\Documents\MSE\MSE\Lab 2\LAB2_Torsional')
  
-m = [2.487 1.262 0.77];
+ I_disk = 0.002348;
+ m = [2.0045 1.004 0];
+ r = 0.09;
+ I_total = I_disk+(m.*r^2);
 %% Question 5
-file_names = ["t3_1_1.txt" "t3_1_2.txt" "t3_1_3.txt"]
+file_names = ["t3_1_1.txt" "t3_1_2.txt" "t3_1_3.txt"];
 
 for i = 1:3;
-  t_firstpeak = peakfind_Q5(file_names(i));
-  t_thirdpeak = peakfind_Q5(file_names(i));
+  t_firstpeak = peakfind_Q5(file_names(i),i);
+  t_thirdpeak = peakfind_Q5(file_names(i),i);
   T = (t_thirdpeak-t_firstpeak)/2;
   delta = 0.5 * log(t_firstpeak/t_thirdpeak);
   dampingRatio = 1/sqrt(1 + ((2*pi)/delta)^2);
   wd = (2*pi)/T;
   wn = wd/sqrt(1 - dampingRatio^2);
-  k = wn^2 * m(i);
-  c = dampingRatio * 2 * sqrt(k*m(i));
+  k = wn^2 * I_total(i);
+  c = dampingRatio * 2 * sqrt(k*I_total(i));
   hold off
   
   fprintf("Damping Coefficient Test %d: %f\n",i,c)
