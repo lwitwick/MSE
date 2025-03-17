d57a447ad249744e88b283ea3bd063587fd832f1
Elliot <eescalan@andrew.cmu.edu>
2025-03-17 13:37:15 -0400

    Question 6 matlab + simulink

--- Lab 2/LAB2_Rectilinear/ode45_SIM.m
+++ Lab 2/LAB2_Rectilinear/ode45_SIM.m
@@ -1,37 +1,64 @@
+global time force m c k
+
+i = 1;
+figure
+masses = [492 492 1717 1717 0 0 1717 1717];
+simsprconst = [300 300 300 500 100 800 100 300];
+simdampconst = [1 5 5 5 30 30 30 80];
+while i < 9
+    string = strcat('t3_2_',num2str(i),'.txt');
+    data = textread(string,'','headerlines',22);
+    dataLength = 5000;
+    tStart = 1001; % lines of offset from movement before drop
+    disp = data(tStart:dataLength,3)';
+    vel = data(tStart:dataLength,8)';
+    time = data(tStart:dataLength,1)' - tStart/1000; %end shifts the data back to 0
+    force = data(tStart:dataLength,2);
+
+    m = (masses(i) + 770)/1000; %numerator in grams
+    c = simdampconst(i);
+    k = simsprconst(i) + 398;
+
+    y0 = [0 0];
+
+    [T,Y] = ode45(@secondorder,time,y0);
+
+    subplot(8,1,i)
+    hold on
+    if i == 1
+        Q1sim = sim('L2Q6_simulink.slx');        
+        plot(Q1sim.displacement.Time, Q1sim.displacement.Data*1000,'Color','#EDB120','LineWidth',1.5)
+    end
+    plot(T, Y(:,1) * 1000,'--','Color','#A2142F','LineWidth',1.5);
+    plot(time, disp,'-.','Color','#0072BD','LineWidth',1.5);
+    xlabel('Time [s]');
+    ylabel('Displacement [mm]');
+    title(strcat("Test ", num2str(i)))
+    if i == 1
+        legend('Simulink Displacement','Matlab Displacement', 'Experimental Displacement', 'Location', 'eastoutside');
+    else
+        legend('Simulated Displacement', 'Experimental Displacement', 'Location', 'eastoutside');
+    end
+    xlim([0 4])
+    ylim([0 13])
+    grid on;
+    i = i + 1;
+end
+
+
 %experiment data
-data = textread('t3_2_1.txt','','headerlines',22);
-dataLength = 5000;
-tStart = 1000; % lines of offset from movement before drop
-disp2 = data(tStart:dataLength,3)';
-vel2 = data(tStart:dataLength,8)';
-time2 = data(tStart:dataLength,1)' - tStart/1000; %end shifts the data back to 0
-% System Parameters
-m = 492/1000; %numerator in grams
-c = 2.445;
-k = 369.354;
-T0 = 1;
-omega_n = sqrt(k/m);
-zeta = c / (2 * sqrt(m * k));
 
-initConds = [0; 0];
-tspan = 0:.001:10-tStart/1000;
 
-f = @(t) T0 * (t >= 0);
+function f = u(t)
+    global time force
+    f = interp1(time,force,t);
+end
 
-% ODE function
-ode_func = @(t, x) [x(2);
-                    (f(t) - c * x(2) - k * x(1)) / m];
+function dq = secondorder(t,q)
+    global m c k
 
-% Solve using ode45
-[t, x] = ode45(ode_func, tspan, initConds);
+    A = [0 1; -k/m -c/m];
+    B = [0; 1/m];
 
-% Plot the response
-figure;
-subplot(2,1,1);
-hold on
-plot(t, x(:,1));
-plot(time2,disp2);
-xlabel('Time (s)');
-ylabel('Displacement (mm)');
-title('Displacement Response of Second-Order System');
-grid on;
+    dq = A*q+B*u(t);
+end
